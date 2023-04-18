3b6ff89301094d8031043704f6031bc34ff85181
VijayShankarReddyHanumanth <x22118608@student.ncirl.ie>
2023-04-18 06:04:40 +0000

    added login system

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
-	
 	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),
+	path('logout/', views.logoutPage, name="logout"),
 ]
