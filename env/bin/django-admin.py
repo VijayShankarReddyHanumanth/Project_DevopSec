dbc4d030c4a2ce6b949cc0a90f4a6ca7d237f8eb
VijayShankarReddyHanumanth <x22118608@student.ncirl.ie>
2023-04-19 00:06:58 +0000

    changed .git root

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ec2-user/environment/Project_DevopSec/ecommerce/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
